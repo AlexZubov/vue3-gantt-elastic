2303e370fa3c46bb5e9a39cf774f8a10df40dcc3
beckho <vngkgk624@gmail.com>
2022-06-13 16:17:44 +0900

    wrap complete vue3

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,251 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    hello gantt
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <GanttElastic :options="options" :tasks="tasks">
+      <template v-slot:header>
+        <GanttElasticHeader2 ></GanttElasticHeader2>
+      </template>
+
+    </GanttElastic>
+  </div>
 </template>
+<script>
+import GanttElastic from "./GanttElastic.vue";
+import dayjs from "dayjs";
+import GanttElasticHeader2 from "./components/GanttElasticHeader2.vue";
 
+export default {
+  components: {
+    GanttElastic,
+    GanttElasticHeader2
+  },
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          label: 'Make some noise',
+          user:
+              '<a href="https://www.google.com/search?q=John+Doe" target="_blank" style="color:#0077c0;">John Doe</a>',
+          start: this.getDate(-24 * 5),
+          duration: 5 * 24 * 60 * 60 * 1000,
+          progress: 85,
+          type: 'project'
+        },
+        {
+          id: 2,
+          label: 'With great power comes great responsibility',
+          user:
+              '<a href="https://www.google.com/search?q=Peter+Parker" target="_blank" style="color:#0077c0;">Peter Parker</a>',
+          parentId: 1,
+          start: this.getDate(-24 * 4),
+          duration: 4 * 24 * 60 * 60 * 1000,
+          progress: 50,
+          type: 'milestone',
+          style: {
+            base: {
+              fill: '#1EBC61',
+              stroke: '#0EAC51'
+            }
+            /*'tree-row-bar': {
+            fill: '#1EBC61',
+            stroke: '#0EAC51'
+          },
+          'tree-row-bar-polygon': {
+            stroke: '#0EAC51'
+          }*/
+          }
+        },
+        {
+          id: 3,
+          label: 'Courage is being scared to death, but saddling up anyway.',
+          user:
+              '<a href="https://www.google.com/search?q=John+Wayne" target="_blank" style="color:#0077c0;">John Wayne</a>',
+          parentId: 2,
+          start: this.getDate(-24 * 3),
+          duration: 2 * 24 * 60 * 60 * 1000,
+          progress: 100,
+          type: 'task'
+        },
+        {
+          id: 4,
+          label: 'Put that toy AWAY!',
+          user:
+              '<a href="https://www.google.com/search?q=Clark+Kent" target="_blank" style="color:#0077c0;">Clark Kent</a>',
+          start: this.getDate(-24 * 2),
+          duration: 2 * 24 * 60 * 60 * 1000,
+          progress: 50,
+          type: 'task',
+          dependentOn: [3]
+        },
+        {
+          id: 5,
+          label: 'One billion, gajillion, fafillion... shabadylu...mil...shabady......uh, Yen.',
+          user:
+              '<a href="https://www.google.com/search?q=Austin+Powers" target="_blank" style="color:#0077c0;">Austin Powers</a>',
+          parentId: 4,
+          start: this.getDate(0),
+          duration: 2 * 24 * 60 * 60 * 1000,
+          progress: 10,
+          type: 'milestone',
+          style: {
+            base: {
+              fill: '#0287D0',
+              stroke: '#0077C0'
+            }
+          }
+        },
+        {
+          id: 6,
+          label: 'Butch Mario and the Luigi Kid',
+          user:
+              '<a href="https://www.google.com/search?q=Mario+Bros" target="_blank" style="color:#0077c0;">Mario Bros</a>',
+          parentId: 5,
+          start: this.getDate(24),
+          duration: 1 * 24 * 60 * 60 * 1000,
+          progress: 50,
+          type: 'task',
+          style: {
+            base: {
+              fill: '#8E44AD',
+              stroke: '#7E349D'
+            }
+          }
+        },
+        {
+          id: 7,
+          label: 'Devon, the old man wanted me, it was his dying request',
+          user:
+              '<a href="https://www.google.com/search?q=Knight+Rider" target="_blank" style="color:#0077c0;">Knight Rider</a>',
+          parentId: 2,
+          dependentOn: [6],
+          start: this.getDate(24 * 2),
+          duration: 4 * 60 * 60 * 1000,
+          progress: 20,
+          type: 'task'
+        },
+        {
+          id: 8,
+          label: 'Hey, Baby! Anybody ever tell you I have beautiful eyes?',
+          user:
+              '<a href="https://www.google.com/search?q=Johhny+Bravo" target="_blank" style="color:#0077c0;">Johhny Bravo</a>',
+          parentId: 7,
+          dependentOn: [7],
+          start: this.getDate(24 * 3),
+          duration: 1 * 24 * 60 * 60 * 1000,
+          progress: 0,
+          type: 'task'
+        },
+        {
+          id: 9,
+          label: 'This better be important, woman. You are interrupting my very delicate calculations.',
+          user:
+              '<a href="https://www.google.com/search?q=Dexter\'s+Laboratory" target="_blank" style="color:#0077c0;">Dexter\'s Laboratory</a>',
+          parentId: 8,
+          dependentOn: [8, 7],
+          start: this.getDate(24 * 4),
+          duration: 4 * 60 * 60 * 1000,
+          progress: 20,
+          type: 'task',
+          style: {
+            base: {
+              fill: '#8E44AD',
+              stroke: '#7E349D'
+            }
+            /*'tree-row-bar-polygon': {
+            stroke: '#7E349D'
+          },
+          'tree-row-bar': {
+            fill: '#8E44AD',
+            stroke: '#7E349D'
+          }*/
+          }
+        },
+        {
+          id: 10,
+          label: 'current task',
+          user:
+              '<a href="https://www.google.com/search?q=Johnattan+Owens" target="_blank" style="color:#0077c0;">Johnattan Owens</a>',
+          start: this.getDate(24 * 5),
+          duration: 24 * 60 * 60 * 1000,
+          progress: 0,
+          type: 'task'
+        }
+      ],
+      options: {
+        title: {
+          label: 'Your project title as html (link or whatever...)',
+          html: false
+        },
+        taskList: {
+          columns: [
+            {
+              id: 1,
+              label: 'ID',
+              value: 'id',
+              width: 40
+            },
+            {
+              id: 2,
+              label: 'Description',
+              value: 'label',
+              width: 200,
+              expander: true
+            },
+            {
+              id: 3,
+              label: 'Assigned to',
+              value: 'user',
+              width: 130,
+              html: true
+            },
+            {
+              id: 3,
+              label: 'Start',
+              value: task => dayjs(task.start).format('YYYY-MM-DD'),
+              width: 78
+            },
+            {
+              id: 4,
+              label: 'Type',
+              value: 'type',
+              width: 68
+            },
+            {
+              id: 5,
+              label: '%',
+              value: 'progress',
+              width: 35,
+              style: {
+                'task-list-header-label': {
+                  'text-align': 'center',
+                  width: '100%'
+                },
+                'task-list-item-value-container': {
+                  'text-align': 'center'
+                }
+              }
+            }
+          ]
+        }
+      }
+    }
+  },
+  methods: {
+    getDate(hours) {
+      const currentDate = new Date();
+      const currentYear = currentDate.getFullYear();
+      const currentMonth = currentDate.getMonth() + 1;
+      const currentDay = currentDate.getDate();
+      const timeStamp = new Date(`${currentYear}-${currentMonth}-${currentDay} 00:00:00`).getTime();
+      return new Date(timeStamp + hours * 60 * 60 * 1000).getTime();
+    }
+  }
+}
+</script>
 <style>
-@import './assets/base.css';
 
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
 </style>
---
+++ src/components/GanttElasticHeader2.vue
@@ -0,0 +1,92 @@
+<template>
+  <div style="margin:16px 0px">
+    <div style="display: flex;align-items: center;justify-content: end">
+      <button @click.prevent="recenterPosition" class="header-btn-recenter">
+        Now
+      </button>
+      <div style="display: flex;align-items: center">
+        <div style="margin:0px 16px">
+          X-Scale
+        </div>
+        <Slider v-model="xScaleValue" :min="2" :max="24" :tooltips="false" style="min-width: 100px"
+                @change="onXScaleChange">
+        </Slider>
+      </div>
+      <div style="display: flex;align-items: center">
+        <div style="margin:0px 16px">
+          Y-Scale
+        </div>
+        <Slider v-model="yScaleValue" :min="7" :max="100" :tooltips="false" style="min-width: 100px"
+                @change="onYScaleChange"></Slider>
+      </div>
+      <div style="display: flex;align-items: center">
+        <div style="margin:0px 16px">
+          Task list
+        </div>
+        <Slider v-model="divider" :min="0" :max="100" :tooltips="false" style="min-width: 100px"
+                @change="onDividerChange"></Slider>
+      </div>
+      <div style="margin-left: 16px">
+        <Toggle v-model="root.state.options.taskList.display">
+
+        </Toggle>
+        showTaskList
+      </div>
+    </div>
+
+
+  </div>
+</template>
+<script>
+import Slider from '@vueform/slider'
+import Toggle from "@vueform/toggle";
+
+export default {
+  components: {
+
+    Slider, Toggle
+  },
+  inject: ["root"],
+  data() {
+    return {
+      xScaleValue: this.root.state.options.times.timeZoom,
+      yScaleValue: this.root.state.options.row.height,
+      before: this.root.state.options.scope.before,
+      divider: this.root.state.options.taskList.percent,
+
+    }
+  },
+  methods: {
+    onXScaleChange(value) {
+      this.$root.emitter.emit("times-timeZoom-change", value);
+    },
+    onYScaleChange(value) {
+      this.$root.emitter.emit("row-height-change", value);
+    },
+    recenterPosition() {
+      this.$root.emitter.emit("recenterPosition")
+    },
+    onBeforeChange(value) {
+      this.$root.emitter.emit("scope-change", value);
+    },
+    onDividerChange(value) {
+      this.$root.emitter.emit("taskList-width-change", value);
+    }
+
+  }
+}
+</script>
+<style>
+.header-btn-recenter {
+  background: #95A5A6;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  color: white;
+  border-radius: 3px;
+  font-size: 16px;
+  padding: 8px 12px
+}
+</style>
+<style src="@vueform/slider/themes/default.css"></style>
+<style src="@vueform/toggle/themes/default.css"></style>
